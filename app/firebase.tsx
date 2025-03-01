a47f5890fa9310d4744c24ba016e771e7f547649
rungruch <rungruch2543@gmail.com>
2025-03-01 19:51:26 +0700

    Create firebase.tsx

---
+++ app/firebase.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDak0XH1Fu6N8a21zyUZm2BbpOuajZST4s",
+  authDomain: "arumstore-inventory.firebaseapp.com",
+  projectId: "arumstore-inventory",
+  storageBucket: "arumstore-inventory.firebasestorage.app",
+  messagingSenderId: "1049773607158",
+  appId: "1:1049773607158:web:508a5d0d32bf60163ac4d7",
+  measurementId: "G-BPJSSK3SX0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
