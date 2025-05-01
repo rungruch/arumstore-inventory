f4a3e130d2c56122653ca72160dcb51b0ebba565
rungruch <rungruch2543@gmail.com>
2025-05-01 16:17:36 +0700

    dark adjust for add payment and shipping

--- components/AddShippingDetail.tsx
+++ components/AddShippingDetail.tsx
@@ -125,8 +125,7 @@ export default function ShippingDetailsForm({
 
   return (
     <div 
-    style={{ backgroundColor: "#00000066" }}
-    className="fixed inset-0 flex justify-center items-center z-50">
+    className="fixed inset-0 flex justify-center items-center z-50 bg-[#00000066] dark:bg-[#00000099]">
       <div ref={modalRef} className="bg-white p-6 rounded-lg w-full max-w-md mx-4 max-h-[90vh] overflow-y-auto dark:bg-zinc-800">
         <h2 className="text-xl font-bold mb-4 top-0">แก้ไขข้อมูลการจัดส่ง</h2>
         
